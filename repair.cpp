90a7f791f7cbd4c4237482c8b006720a591b9fdb
OscarDebra <osc.debra@gmail.com>
2025-12-15 10:27:02 +0100

    Made the code return to last decision point instead of just backtracking one tile at a time, also added the graph class and buildgraph function to build an adjacency list, but it has not been implemented yet

--- repair.cpp
+++ repair.cpp
@@ -7,14 +7,60 @@
 
 using namespace std;
 
-int gridWidth  = 4;
-int gridHeight = 4;
-int totalSquares = 16;
+int gridWidth  = 6;
+int gridHeight = 5;
+int totalSquares = 30;
 deque <Vector2> visited = {};
 
 
 Vector2 startCoord = Vector2{0, 0};
 Vector2 endCoord = Vector2{0, 1};
+
+class Graph {
+private:
+    map <Vector2, vector <Vector2>, Vector2Compare> adjacencyList;
+    set <Vector2, Vector2Compare> vertices;
+
+public:
+    // Add a vertex to the graph
+    void addVertex(Vector2 v) {
+        vertices.insert(v);
+        if (adjacencyList.find(v) == adjacencyList.end()) {
+            adjacencyList[v] = {};
+        }
+    }
+
+    // Add an edge between two vertices (bidirectional)
+    void addEdge(Vector2 a, Vector2 b) {
+        adjacencyList[a].push_back(b);
+        adjacencyList[b].push_back(a);
+    }
+
+    // Get neighbors of a vertex
+    vector <Vector2> getNeighbors(Vector2 v) const {
+        auto it = adjacencyList.find(v);
+        if (it != adjacencyList.end()) {
+            return it->second;
+        }
+        return {};
+    }
+
+    // Get all vertices
+    set <Vector2, Vector2Compare> getVertices() const {
+        return vertices;
+    }
+
+    // Check if vertex exists
+    bool hasVertex(Vector2 v) const {
+        return vertices.find(v) != vertices.end();
+    }
+
+    // Get degree (number of neighbors)
+    int degree(Vector2 v) const {
+        auto it = adjacencyList.find(v);
+        return (it != adjacencyList.end()) ? it->second.size() : 0;
+    }
+};
 
 
 
@@ -24,6 +70,36 @@ struct Vector2Compare {
         return a.y < b.y;
     }
 };
+
+Graph BuildGraph() {
+    Graph g;
+
+    vector<Vector2> shape = {
+        {0, 0}, {1, 0}, {2, 0}, {3, 0},
+        {0, 1}, {1, 1}, {2, 1}, {3, 1},
+        {0, 2}, {1, 2}, {2, 2}, {3, 2},
+        {0, 3}, {1, 3}, {2, 3}, {3, 3}
+    };
+    
+    // Add all vertices
+    for (Vector2 v : shape) {
+        g.addVertex(v);
+    }
+    
+    // Add edges based on adjacency (4-directional)
+    for (Vector2 v : shape) {
+        Vector2 directions[] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        
+        for (Vector2 dir : directions) {
+            Vector2 neighbor = Vector2Add(v, dir);
+            if (g.hasVertex(neighbor)) {
+                g.addEdge(v, neighbor);
+            }
+        }
+    }
+    
+    return g;
+}
 
 
 
@@ -85,16 +161,17 @@ int AmountOfValidNeighbors(Vector2 coord) {
 deque <Vector2> FindHamiltonianPath() {
     Vector2 currentCoord = startCoord;
     visited.push_back(startCoord);
+    deque <Vector2> lastDecisionPoints = {startCoord};
 
     map <Vector2, deque <Vector2>, Vector2Compare> availableMoves;
 
     while (visited.size() < totalSquares || currentCoord != endCoord) {
 
-        if (availableMoves.find(currentCoord) == availableMoves.end()) { // Compute available moves for coordinate if not already done
+        if (availableMoves.find(currentCoord) == availableMoves.end()) { // Compute available moves for coordinate if not already done.
             availableMoves[currentCoord] = ValidNeighbors(currentCoord);
         }
 
-        deque <Vector2>& moves = availableMoves[currentCoord];
+        deque <Vector2>& moves = availableMoves[currentCoord]; // This is a reference and not a copy because of the "&" symbol.
 
         cout << "current: " << "(" << currentCoord.x << ", " << currentCoord.y << ")" << " Available: ";
         for (Vector2 move : moves) {
@@ -103,7 +180,12 @@ deque <Vector2> FindHamiltonianPath() {
         cout << endl;
 
         if (!moves.empty()) {
+            int AmountOfValidMoves = moves.size();
 
+            if (AmountOfValidMoves >= 2) {
+                lastDecisionPoints.push_back(currentCoord);
+            }
+            
             int fewestValidNeighbors = INT_MAX;
             Vector2 bestMove = {};
             int bestMoveIdx = -1;
@@ -119,7 +201,6 @@ deque <Vector2> FindHamiltonianPath() {
             }
 
             moves.erase(availableMoves[currentCoord].begin() + bestMoveIdx);
-
             visited.push_back(bestMove);
             currentCoord = bestMove;
         }
@@ -130,11 +211,17 @@ deque <Vector2> FindHamiltonianPath() {
             }
             
             cout << "Backtracking from (" << currentCoord.x << ", " << currentCoord.y << ")" << endl;
-            
-            availableMoves.erase(currentCoord); // Clear this position's moves, for if we come back to this tile via a different path.
-            
-            visited.pop_back();
-            currentCoord = visited.back();
+
+            // Remove data on the tiles that we are backtracking past while going to the last decision point.
+            while (visited.back() != lastDecisionPoints.back()) {
+                cout << "Erasing: " << "(" << visited.back().x << ", " << visited.back().y << ") " << endl;
+
+                availableMoves.erase(visited.back());
+                visited.pop_back();
+            }
+
+            currentCoord = lastDecisionPoints.back();
+            lastDecisionPoints.pop_back();
         }
     }
 
@@ -154,6 +241,7 @@ deque <Vector2> FindHamiltonianPath() {
 
 int main() {
     deque <Vector2> path = FindHamiltonianPath();
+    Graph graph = BuildGraph();
 
     for (auto coord : path) {
         cout << "(" << coord.x << ", " << coord.y << ")" << endl;
